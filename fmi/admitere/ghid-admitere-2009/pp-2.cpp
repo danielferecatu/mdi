cd3869506bcd093f7a9b0482fb4afb4170837c2b
Daniel Ferecatu <danvali657@gmail.com>
2015-07-14 16:48:13 +0300

    quick

---
+++ fmi/admitere/ghid-admitere-2009/pp-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int fact(int s, int e) {
+
+    if( e>=s ) return e*fact(s, e-1);
+    else return 1;
+
+}
+
+int comb(int n, int k) {
+
+    return fact(n-k+1, n) / fact(2, k);
+
+}
+
+int main() {
+
+// variabile
+char par;
+int d=0, i=0;
+
+// citire date
+cout << "Dati secventa de paranteze: ";
+do {
+    cin >> par;
+    if( par=='(' ) d++;
+    else if( par==')' ) i++;
+}
+while( par=='(' || par==')' );
+
+// afisare rezultat
+if( d==i ) cout << endl << "Sirul este corect";
+else cout << endl << "Sirul nu este corect";
+
+}
---
+++ fmi/admitere/ghid-admitere-2009/pp-3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int fact(int s, int e) {
+
+    if( e>=s ) return e*fact(s, e-1);
+    else return 1;
+
+}
+
+int comb(int n, int k) {
+
+    return fact(n-k+1, n) / fact(2, k);
+
+}
+
+int main() {
+
+// variabile
+int n=0, k=0;
+
+// citire date
+cout << "n= ";
+cin >> n;
+cout << "k= ";
+cin >> k;
+
+// afisare rezultat
+cout << endl << "C= " << comb(n, k);
+
+}
