5eada4e8b8ae0a9835a673fe1b402f7ebb7d7e75
Daniel Ferecatu <danvali657@gmail.com>
2015-07-21 07:40:36 +0300

    19 iulie

---
+++ fmi/admitere/2015/1-a.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// variabile globale
+int a, b, c, n, s[1000];
+
+// verifica daca este pereche
+bool pereche(int x, int y) {
+
+    if( a*x*x + b*y*y == c ) return 1;
+    else return 0;
+
+}
+
+int main() {
+
+// citire date
+cout << "a= "; cin >> a;
+cout << "b= "; cin >> b;
+cout << "c= "; cin >> c;
+cout << "n= "; cin >> n;
+for (int i=0; i<n; i++) {
+    cout << "s[" << i << "]= ";
+    cin >> s[i];
+}
+
+// procesare date si afisare
+for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+        if( pereche(s[i], s[j]) ) {
+            cout << "(" << s[i] << "," << s[j] << ")" << endl;
+        }
+    }
+}
+
+return 0;
+
+}
---
+++ fmi/admitere/2015/1-b.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+// variabile globale
+int a, b, c, n, s[1000];
+
+// verifica daca este pereche
+bool pereche(int x, int y) {
+
+    if( a*x*x + b*y*y == c ) return 1;
+    else return 0;
+
+}
+
+int main() {
+
+// numarul de perechi
+int nr=0;
+
+// citire date
+cout << "a= "; cin >> a;
+cout << "b= "; cin >> b;
+cout << "c= "; cin >> c;
+cout << "n= "; cin >> n;
+for (int i=0; i<n; i++) {
+    cout << "s[" << i << "]= ";
+    cin >> s[i];
+}
+
+// procesare date si afisare
+for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+        if( pereche(s[i], s[j]) ) {
+            nr++;
+        }
+    }
+}
+
+cout << nr;
+
+return 0;
+
+}
